70917680d6533a2fd96fe1b822c5bff1fc41c7ad
Ishita Tiwari <ishitatiwari060@gmail.com>
2026-06-23 22:25:27 +0530

    Update API URL for production

--- client/vite-project/src/services/api.js
+++ client/vite-project/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // sends req to backend
 const API = axios.create({
-    baseURL: "http://127.0.0.1:8000"
+    baseURL: "https://movie-recommendation-system-urau.onrender.com"
 });
 
 export default API;
